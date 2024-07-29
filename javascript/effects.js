8688184a02061fd7d3190405a9462c4e6742a58e
Air Li <air1507@localhost.localdomain>
2024-07-29 10:40:08 +0800

    add some responsive css

--- javascript/effects.js
+++ javascript/effects.js
@@ -2,5 +2,5 @@ function loadNeofetch() {
     $('div#info').hide();
     setTimeout(() => {
         $('div#info').fadeIn(500);
-    }, 6100);
+    }, 3100);
 }
--- javascript/page.js
+++ javascript/page.js
@@ -6,7 +6,7 @@ function page(number) {
         setTimeout(() => {
             $('main').load("pages/home");
         }, 300);
-        console.log('Stage 1 - Loading selected page: Home');
+        console.log('Stage 1 - Loading page: Home');
 
         setTimeout(() => {
             $('#card1').fadeIn(300);
@@ -18,7 +18,7 @@ function page(number) {
             $('#options').fadeIn(300);
             $('#options').css('display','flex');
             console.log('Stage 3 - Fading in page "Home" Card1 Options List');  
-        }, 7250);
+        }, 4250);
     } else if (number == 1) {
         $('main').load("pages/blog");
     }
@@ -29,10 +29,10 @@ function terminalOption(number) {
         $('.cards').fadeOut(400);
         setTimeout(() => {
             $('main').load("pages/readme");
-        }, 550);
+        }, 600);
         setTimeout(() => {
             $('#card2').fadeIn(600);
-        }, 620);
+        }, 700);
     } else if (number == 2) {
         page(1);
     } else {
